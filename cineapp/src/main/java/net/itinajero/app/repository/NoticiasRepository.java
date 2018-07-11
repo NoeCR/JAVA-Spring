e6feb4ab710b399d5e3503c810c23745c4ccf0e5
Noé <alu20468585b@ieselcaminas.org>
2018-07-11 16:03:13 +0200

    Tema 13, Query Methods, creacion de paquete pruebas query e implementacion de metodos Query en Noticiasrepository

--- cineapp/src/main/java/net/itinajero/app/repository/NoticiasRepository.java
+++ cineapp/src/main/java/net/itinajero/app/repository/NoticiasRepository.java
@@ -1,5 +1,8 @@
 package net.itinajero.app.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +13,24 @@ import net.itinajero.app.model.Noticia;
 //public interface NoticiasRepository extends CrudRepository<Noticia, Integer> {
 public interface NoticiasRepository extends JpaRepository<Noticia, Integer> {
 
+	// select * from Noticias
+	List<Noticia> findBy();
+	
+	// select * from noticia where estatus = ?
+	List<Noticia> findByEstatus(String estatus);
+	
+	// select * from Noticias where fecha = ?
+	List<Noticia> findByFecha(Date fecha);
+	
+	//select * from Noticias where estatus= ? and fecha =?
+	List<Noticia> findByEstatusAndFecha(String estatus,Date fecha);
+	
+	//select * from Noticias where estatus= ? or fecha =?
+	List<Noticia> findByEstatusOrFecha(String estatus,Date fecha);
+	
+	// where fecha between ? and ?
+	List<Noticia> findByFechaBetween(Date fecha_ini,Date fecha_fin);
+	
+	// where id between ? and ?
+	List<Noticia> findByIdBetween(int n1, int n2);
 }
